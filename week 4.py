2ce823a87b3133213167a51b7c82e30a5f3ed7e9
Weronika Swistowska <143666671+maevemoon@users.noreply.github.com>
2023-10-11 16:02:36 +0100

    Update week 4.py

--- week 4.py
+++ week 4.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 path = 'week 4 data/'
 
 # treating strings and ints differently in data
@@ -26,3 +29,11 @@ for line in open(path+'sample6.txt'):
     items = line.rstrip('\n').split(' ')
     data[items[0]] = [eval(i) for i in items[1:]] # int() doesn't work here when there are multiple entries, so eval()
 print(data)
+
+# numpy vs pandas (delimiter -> sep)
+npdata = np.loadtxt(path+'array.txt',delimiter=',') 
+pddata = pd.read_csv(path+'sample_dat.dat',sep='\t') # \t = tab
+
+# exercise 5
+covid = pd.read_excel(path + 'covid-cases.xlsx', skiprows=[0,1,2], index_col='date') #skiprows so we can get index column
+print(covid.tail(10))
